db40915b3af59bae3b38daee2e247af692aa16c9
min <mjk12300@naver.com>
2025-07-22 00:02:08 +0900

    6. Array 실습
    -센서 그리드 데이터 (예: 검사기 좌표별 OK/NG 결과)
    포지션 맵 (예: X, Y 위치별 파라미터 저장)
    시퀀스 테이블 (예: 각 공정별 설정 값)

---
+++ ArrayStudy/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 3x3 검사 결과 매트릭스(1:OK, 0: NG)
+        int[,] inspectionResult = new int[,]
+        {
+            {1, 1, 0 },
+            {1, 0, 1 },
+            {1, 1, 1 }
+        };
+
+        // 결과 전체 출력 (디버깅용)
+        Console.WriteLine("검사 결과 매트릭스");
+        for (int row = 0; row < inspectionResult.GetLength(0); row++)
+        {
+            for (int col = 0; col < inspectionResult.GetLength(1); col++)
+            {
+                Console.WriteLine($"{inspectionResult[row, col]}");
+            }
+            Console.WriteLine();
+        }
+
+        // 특정 포인트 확인
+        int result = inspectionResult[1, 2]; // 인덱스는 0부터
+        string status = result == 1 ? "OK" : "NG";
+        Console.WriteLine($"2행 3열 결과: {status}");
+
+        Console.WriteLine("\n NG 위치 목록");
+        for (int row = 0; row < inspectionResult.GetLength(0); row++)
+        {
+            for (int col = 0; col < inspectionResult.GetLength(1); col++)
+            {
+                if (inspectionResult[row, col] == 0)
+                {
+                    Console.WriteLine($"-> NG 발견: {row + 1}행 {col + 1}열 (index: [{row},{col}])");
+                }
+            }
+        }
+    }
+}
