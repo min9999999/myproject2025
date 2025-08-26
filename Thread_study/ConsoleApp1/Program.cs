a799ca1bd690eea972f25fce32b45e6a61c87891
min <mjk12300@naver.com>
2025-08-26 23:13:44 +0900

    8. Thread 실습
    join과 wait를 적절히 사용하여 항상 출력 순서가 일정하고 깔끔하게 정리하자.

---
+++ Thread_study/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void DoSomething()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"DoSomething : {i}");
+                Thread.Sleep(10);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(DoSomething));
+
+            Console.WriteLine("Start thread...");
+            t1.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Main : {i}");
+                Thread.Sleep(10);
+            }
+
+            Console.WriteLine("Wating until thread stops...");
+            t1.Join();
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
